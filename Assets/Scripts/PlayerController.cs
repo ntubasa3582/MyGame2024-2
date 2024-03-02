ceddbc5c3dcfe1e80350e4272b750aa4f45cc98a
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-03-02 23:53:48 +0900

    プレイヤーからエフェクトを生成できるようにした

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] List<GameObject> _effects = new List<GameObject>();
+    [SerializeField] GameObject _effectInstancePos = default;               //エフェクトの生成座標になるゲームオブジェクト
     [SerializeField] float _speed = 3f;
     [SerializeField] float _jumpParameter = 1f;
-    Rigidbody _rb;
-    Vector3 _pos;
+    Rigidbody _rb = default;
+    Vector3 _pos = default;
+    readonly int _effectCount = 0;                                          //エフェクトの順番をカウントする変数
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
@@ -13,20 +17,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        _pos = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")); //キー入力の値を変数に入れる
+        _pos = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));    //プレイヤーの移動処理
         if (_pos.magnitude > 0 )
         {
-            _pos = Camera.main.transform.TransformDirection(_pos);
+            _pos = Camera.main.transform.TransformDirection(_pos);  //カメラからの角度で座標に変換する
             _pos.y = 0;
             transform.LookAt(transform.position + _pos);
 
             float verticalVelocity = _rb.velocity.y;
             _rb.velocity = _pos.normalized * _speed + Vector3.up * verticalVelocity;
         }
-        if (Input.GetButtonDown("Jump"))
+
+        if (Input.GetButtonDown("Jump"))    //プレイヤーのジャンプ処理
         {
             _rb.velocity = new Vector3(_rb.velocity.x, _jumpParameter, _rb.velocity.z);
         }
+
+        if (Input.GetButtonDown("Fire1"))   //エフェクトを生成する
+        {
+            Instantiate(_effects[_effectCount], _effectInstancePos.transform.position, Quaternion.identity);
+        }
+
         Cursor.visible = false;
     }
 }
