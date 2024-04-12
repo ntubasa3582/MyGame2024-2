2408687d88894cec3ca13afe0f85ba393bd92012
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-04-12 22:46:25 +0900

    MoneyUIの変更

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] _pausePanel = default;
+    PauseGame _pauseGame;
+    void Awake()
+    {
+        _pauseGame = FindAnyObjectByType<PauseGame>();
+        for (int i = 0; i < _pausePanel.Length; i++)
+        {
+            _pausePanel[i].SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        //デリゲート登録
+        _pauseGame.OnPauseResume += PauseResume;
+    }
+
+    private void OnDisable()
+    {
+        //デリゲート解除
+        _pauseGame.OnPauseResume -= PauseResume;
+    }
+
+    void PauseResume(bool isPause)
+    {
+        //オブジェクトのセットアクティブを切り替える
+        if (isPause)
+        {
+            for (int i = 0; i < _pausePanel.Length; i++)
+            {
+                _pausePanel[i].SetActive(true);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < _pausePanel.Length; i++)
+            {
+                _pausePanel[i].SetActive(false);
+            }
+        }
+    }
+}
