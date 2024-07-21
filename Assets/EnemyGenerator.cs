1ecf7673783310c2cbfcc56cb191688a654074a9
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-07-21 18:47:17 +0900

    リポジトリの作成

---
+++ Assets/EnemyGenerator.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject _enemyPrefab;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
